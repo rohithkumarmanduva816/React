3f8e7e4c4ca770711bf738825fd085d18f10045e
rohith kumar <rmmmz@mail.umkc.edu>
2020-02-03 22:21:20 -0600

    login-application

---
+++ homepage/src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <p>This is mostly about us.</p>
+    <p>Marriage, also called matrimony or wedlock, is a culturally recognised union between people, called spouses, that establishes rights and obligations between them, as well as between them and their children, and between them and their in-laws.[1] The definition of marriage varies around the world, not only between cultures and between religions, but also throughout the history of any given culture and religion. Over time, it has expanded and also constricted in terms of who and what is encompassed. Typically, it is an institution in which interpersonal relationships, usually sexual, are acknowledged or sanctioned. In some cultures, marriage is recommended or considered to be compulsory before pursuing any sexual activity. When defined broadly, marriage is considered a cultural universal. A marriage ceremony is called a wedding.</p>
+    <p>Marriage can be recognized by a state, an organization, a religious authority, a tribal group, a local community, or peers. It is often viewed as a contract. When a marriage is performed and carried out by a government institution in accordance with the marriage laws of the jurisdiction, without religious content, it is a civil marriage. Civil marriage recognizes and creates the rights and obligations intrinsic to matrimony in the eyes of the state. When a marriage is performed with religious content under the auspices of a religious institution, it is a religious marriage. Religious marriage recognizes and creates the rights and obligations intrinsic to matrimony in the eyes of that religion. Religious marriage is known variously as sacramental marriage in Catholicism, nikah in Islam, nissuin in Judaism, and various other names in other faith traditions, each with their own constraints as to what constitutes, and who can enter into, a valid religious marriage.</p>
+  </div>
+
+)
---
+++ homepage/src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Home = () => (
+  <div>
+    <h2>Marriage</h2>
+    
+    <p>RK match making was founded within startup incubator Hatch Labs by Sean Rad, Jonathan Badeen, Justin Mateen, Joe Munoz, Dinesh Moorjani, and Whitney Wolfe, the latter of whom left Tinder to create Bumble in 2014.[12][13] Other sources restrict the list of founders to Mateen, Rad, and Badeen,[14][15] though this has been disputed.[16]
+
+Sean Rad and Justin Mateen had known each other from the age of 14. Both come from Jewish-Iranian families in the Los Angeles area,[17][18] and both attended the University of Southern California and became online entrepreneurs at the same time. Rad has said the impetus for Tinder's creation was his observation that "no matter who you are, you feel more comfortable approaching somebody if you know they want you to approach them." He believed a "double opt-in" system could be created to potentially alleviate the stress of meeting new people. Rad has also said Tinder filled a gap in the availability of social platforms for meeting strangers, rather than connecting with people a user already knows.[14][19]
+
+Tinder was seeded at numerous college campuses and quickly expanded to additional campuses. The app won TechCrunch's Crunchie Award for "Best New Startup of 2013."[12]
+
+In March 2013, Tinder's social media director Alexa Mateen, who spearheaded Tinder's college campus expansion, said the app offered a "chance to meet people you wouldn't normally meet".[20]
+
+By May 2013, Tinder was one of the top 25 social networking apps online, based on frequency of use and number of users. Initially, instead of utilizing Tinder's current swiping motion for making matches, users had to click either a green heart or a red X to select or move on from a displayed photo.[21] Tinder became the first new online dating service to claim a spot as one of the web's top five utilized services in about 10 years.[22]
+
+Sean Rad has served as Tinder's CEO on two occasions. He was the company's initial CEO until March 2015, when he was replaced by former eBay and Microsoft executive Chris Payne. Payne left the company, and Rad simultaneously returned as CEO, in August 2015.[23] Rad stepped down as CEO again in December 2016, becoming Tinder's chairman and ceding the CEO position to former Tinder chairman Greg Blatt.[24]</p>
+  </div>
+)
